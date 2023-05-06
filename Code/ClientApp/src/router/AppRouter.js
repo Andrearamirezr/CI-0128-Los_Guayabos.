d56307d186779b879f14702875e561384808bbdc
RANDALL ESTEBAN LOPEZ VARELA <randall.lopezvarela@ucr.ac.cr>
2023-05-06 00:15:28 -0600

    Correcciones al AppRouter.

--- Code/ClientApp/src/router/AppRouter.js
+++ Code/ClientApp/src/router/AppRouter.js
@@ -16,7 +16,16 @@ const AppRouter = () => {
                     <Route path="dashboard" element={<Dashboard />} />
                     <Route path="inventario" element={<Inventario />} />
                     <Route path="inventario/agregar" element={<AgregarProducto />} />
+                    { /*<Route path="inventario/editar:sku" element={<EditarProducto />} />*/}
+                    { /*<Route path="inventario/producto:sku" element={<VerProducto />} />*/}
+                    { /*<Route path="ordenes" element={<Ordenes />} />*/}
+                    { /*<Route path="ordenes/agregar" element={<AgregarOrden />} />*/}
+                    { /*<Route path="ordenes/editar:id" element={<EditarOrden />} />*/}
+                    { /*<Route path="ordenes/orden:id" element={<VerOrden />} />*/}
                     { /*<Route path="clientes" element={<Clientes />} />*/}
+                    { /*<Route path="clientes/agregar" element={<AgregarCliente />} />*/}
+                    { /*<Route path="clientes/editar:id" element={<EditarCliente />} />*/}
+                    { /*<Route path="clientes/orden:id" element={<VerCliente />} />*/}
                     { /*<Route path="agenda" element={<Agenda />} />*/}
                 </Route>
                 <Route path="login" element={<Login />} />
